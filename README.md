e66fea8e67fdc88c3227f95e9085e2d06fbf52d8
Ny <44008409+FourWheelDrive@users.noreply.github.com>
2023-12-01 17:39:47 -0700

    Initial commit from Sirius.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# fourwheeldrive.github.io
-Profile website repository
+# Project-Sirius
+Personal project website. I am planning to introduce more rigorous responsive CSS for different screen resolutions soon.
+
+Hello! I'm Jeffery. I like to use HTML5 to make games.
---
+++ main.js
@@ -0,0 +1,146 @@
+class windowManager {
+    constructor() {
+        this.windowState = -1;  //-1 is the about page.
+        this.currentWindowDOMid = "pageBody__about";
+
+        this.transitionBox = document.getElementById("transitionBox");
+        this.projectButtons = document.getElementsByClassName("project-menu-button");
+        this.homeButton = document.getElementById("navMenu__home");
+
+        this.transformDuration = "1.5s";
+        this.fadeDuration = "0.75s";
+    }
+
+    //switch window.
+    async changeWindow(button_id) {
+        //disable buttons.
+        for (let i = 0; i < this.projectButtons.length; i++) {
+            this.projectButtons[i].disabled = true;
+        }
+        this.homeButton.disabled = true;
+
+        let nextWindowIndex;
+        //case 1: home button
+        if (button_id == "navMenu__home") {
+            nextWindowIndex = -1;
+
+            //Remove last window
+            await this.windowFadeOut(this.currentWindowDOMid);
+
+            //Enabling buttons here for better responsitivity.
+            //enable buttons.
+            for (let i = 0; i < this.projectButtons.length; i++) {
+                this.projectButtons[i].disabled = false;
+            }
+            this.homeButton.disabled = false;
+
+            //Show new window
+            this.windowFadeIn("pageBody__about");
+
+            this.currentWindowDOMid = "pageBody__about";
+            //case 2: project buttons
+        } else {
+            nextWindowIndex = button_id.slice(-1);
+
+            //Remove last window
+            await this.windowFadeOut(this.currentWindowDOMid);
+
+            //Enabling buttons here for better responsitivity.
+            //enable buttons.
+            for (let i = 0; i < this.projectButtons.length; i++) {
+                this.projectButtons[i].disabled = false;
+            }
+            this.homeButton.disabled = false;
+
+            //Show new window
+            this.windowFadeIn(`pageBody__projects__${nextWindowIndex}`);
+
+            this.currentWindowDOMid = `pageBody__projects__${nextWindowIndex}`;
+        }
+
+        this.windowState = nextWindowIndex;
+    }
+
+    async windowFadeOut(id) {
+        //transition this to slide over the screen, then hide the previous page.
+        this.transitionBox.style.transitionDuration = this.transformDuration;
+        flushCSS(this.transitionBox);
+        //not sure why i need 2 await sleeps here.
+        await sleep(1);
+        this.transitionBox.style.transform += `translateX(-101vw)`;
+        await sleep(2000);
+
+        //Hide the last window
+        document.getElementById(id).style.display = "none";
+        //Move transition box back.
+        this.transitionBox.style.transitionDuration = "0s";
+        flushCSS(this.transitionBox);
+        this.transitionBox.style.transform = `translateX(0)`;
+    }
+
+    async windowFadeIn(id) {
+        let element;
+        //Toggle display.
+        if (id == "pageBody__about") {
+            element = document.getElementById("pageBody__about");
+            element.style.transitionDuration = "0s";
+            flushCSS(element);
+            element.style.opacity = "0.0";
+            element.style.display = "inline";
+        } else {
+            element = document.getElementById(id);
+            element.style.transitionDuration = "0s";
+            flushCSS(element);
+            element.style.opacity = "0.0";
+            element.style.display = "grid";
+        }
+        //Fade in.
+        element.style.transitionDuration = this.fadeDuration;
+        flushCSS(element);
+        element.style.opacity = "1.0";
+        await sleep(1000);
+    }
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+function flushCSS(element) { //flushes css to no transition.
+    element.offsetHeight;
+}
+
+function onload() {
+    const handler = new windowManager();
+
+    //initialize project buttons.
+    let projectButtons = document.getElementsByClassName("project-menu-button");
+    for (let i = 0; i < projectButtons.length; i++) {
+        projectButtons[i].addEventListener("click", () => { handler.changeWindow(projectButtons[i].id) })
+    }
+    document.getElementById("navMenu__home").addEventListener("click", () => { handler.changeWindow("navMenu__home") });
+}
+
+/*
+function initializeDots(){
+    var dots = document.getElementsByClassName("dot");
+    for(let i = 0; i < dots.length; i++){
+        //Check which way I want the lines to go. Draw a line to the edge of the screen.
+        if(dots.item(i).classList.contains("right")){
+            drawLine()
+        }
+        if(dots.item(i).classList.contains("left")){
+
+        }
+    }
+
+    function drawLine(dotPos){
+        
+    }
+}
+
+//https://stackoverflow.com/questions/44109314/javascript-calculate-with-viewport-width-height
+function vh(percent) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (percent * h) / 100;
+}
+*/
